0215b4480bd4c9f749983d3c7d6ce33ae1163d61
Danny Rankevich <dannyr@similargroup.com>
2015-08-18 03:23:15 +0300

    finish site

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/**
+ * Created by dannyr on 8/18/2015.
+ */
+
+var app = angular.module('ng-tip-site', []);
+
+
+app.controller('ngTooltipCtrl', function ($scope) {
+    var ctrl = this;
+
+    var dirs = ['top', 'bottom', 'left', 'right' /* 'top-left', 'top-right', 'bottom-left', 'bottom-right' */];
+
+    $scope.tooltips = [];
+
+    for (var i = 0; i < dirs.length; i++) {
+        var popupData = {
+            opened: false,
+            onHover: i%2==0,
+            config: {
+                width: 300,
+                height: 150,
+                templateUrl: 'sandbox-tooltip.html',
+                controller: 'ngTooltipTestCtrl',
+                placement: dirs[i],
+                appendToBody: false,
+                isolateScope: true
+            }
+        };
+
+        $scope.tooltips.push(popupData);
+    }
+
+    $scope.setPopupOpened = function (index, val) {
+        var popupData = $scope.tooltips[index];
+        if (!angular.isDefined(val))
+            val = !popupData.opened;
+        popupData.opened = val;
+    };
+});
+
+app.controller('ngTooltipTestCtrl', function ($scope, $tooltipInstance) {
+    $scope.title = "this is the tooltip title";
+    $scope.close = function () {
+        $tooltipInstance.close();
+    }
+});
